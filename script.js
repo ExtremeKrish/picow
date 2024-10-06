cb5da561f94c1e99988d059af8eec7b9fdd272c6
Krish Vishwakarma <79414142+ExtremeKrish@users.noreply.github.com>
2024-10-07 01:08:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-var hello = 1;
+document.getElementById('newbtn').addEventListener('click', function() {
+    let newName = prompt("Enter new name:");
+    if (newName) {
+        // Code to handle new name creation can be added here
+        alert("New file created: " + newName);
+    }
+});
